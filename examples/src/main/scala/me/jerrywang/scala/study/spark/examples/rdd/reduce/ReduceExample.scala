e060fad66fade8e7ef19034a0811b3bcfc725d7e
Jerry Wang <j3rry.wan9@gmail.com>
2018-08-31 11:06:18 -0700

    add an example for RDD reduce action

---
+++ examples/src/main/scala/me/jerrywang/scala/study/spark/examples/rdd/reduce/ReduceExample.scala
@@ -0,0 +1,17 @@
+package me.jerrywang.scala.study.spark.examples.rdd.reduce
+
+import org.apache.log4j.{ Level, Logger }
+import org.apache.spark.SparkContext
+
+object ReduceExample {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    val sparkContext = new SparkContext("local[*]", "ReduceExample")
+
+    val inputIntegers = List(1, 2, 3, 4, 5)
+    val integerRDD = sparkContext.parallelize(inputIntegers)
+
+    val product = integerRDD.reduce(_ * _)
+    println("Product is :" + product)
+  }
+}
